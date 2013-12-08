913dbdfca8ce664b6cf5a9cf969b755012141b11
Benjamin Althues <benjamin@babab.nl>
2013-12-08 22:31:59 +0100

    Map passvigor outside of script; Add progressbar

--- passvigor-main.js
+++ passvigor-main.js
@@ -14,93 +14,82 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-$(document).ready(function() {
-    function passvigor(password)
+function passvigor(password)
+{
+    var passlenreq = 9;
+    var maxstrength = 299;
+    var ret = {
+        'valid': false,
+        'points': 0,
+        'message': null
+    };
+
+    function relscore(password)
     {
-        var passlenreq = 9;
-        var maxstrength = 299;
-        var ret = {
-            'valid': false,
-            'points': 0,
-            'message': null
-        };
+        var lowercase = /[a-z]/
+        var uppercase = /[A-Z]/
+        var digits = /[0-9]/
+        var special = /[!@#$%^&*()_\-<>?/\\\[\]]/
+        var factor = 0;
 
-        function relscore(password)
-        {
-            var lowercase = /[a-z]/
-            var uppercase = /[A-Z]/
-            var digits = /[0-9]/
-            var special = /[!@#$%^&*()_\-<>?/\\\[\]]/
-            var factor = 0;
+        if (lowercase.test(password))
+            factor += 26 / (26 + 26 + 10);
+        if (uppercase.test(password))
+            factor += 26 / (26 + 26 + 10);
+        if (digits.test(password))
+            factor += 10 / (26 + 26 + 10);
+        if (special.test(password))
+            factor += 9 / (26 + 26 + 10);
+        return factor;
 
-            if (lowercase.test(password))
-                factor += 26 / (26 + 26 + 10);
-            if (uppercase.test(password))
-                factor += 26 / (26 + 26 + 10);
-            if (digits.test(password))
-                factor += 10 / (26 + 26 + 10);
-            if (special.test(password))
-                factor += 9 / (26 + 26 + 10);
-            return factor;
+    }
 
-        }
-
-        if ($.inArray(password, passvigor_wordlist) != -1) {
-            ret['message'] = 'Unsecure password: very commonly used.';
+    if ($.inArray(password, passvigor_wordlist) != -1) {
+        ret['message'] = 'Unsecure password: very commonly used.';
+        return ret;
+    }
+    else {
+        if (password.length == 0) {
+            ret['message'] = 'Enter a password.';
             return ret;
         }
-        else {
-            if (password.length == 0) {
-                ret['message'] = 'Enter a password.';
+
+        var relp = relscore(password);
+        var currp = Math.round((password.length / passlenreq) * 100);
+        var points = 0;
+        if (relp < 1 && currp > (relp * 100))
+            points = Math.round(relp * 100);
+        else
+            points = Math.ceil(currp * relp);
+
+        if (points >= (maxstrength + 1))
+            points = maxstrength;
+
+        ret['points'] = points;
+
+        if (points < 100) {
+            if (relp >= 1) {
+                ret['message'] = 'Still need '
+                        + (passlenreq - password.length) + ' chars.';
                 return ret;
             }
-
-            var relp = relscore(password);
-            var currp = Math.round((password.length / passlenreq) * 100);
-            var points = 0;
-            if (relp < 1 && currp > (relp * 100))
-                points = Math.round(relp * 100);
-            else
-                points = Math.ceil(currp * relp);
-
-            if (points >= (maxstrength + 1))
-                points = maxstrength;
-
-            ret['points'] = points;
-
-            if (points < 100) {
-                if (relp >= 1) {
-                    ret['message'] = 'Still need '
-                            + (passlenreq - password.length) + ' chars.';
-                    return ret;
-                }
-                else if (passlenreq <= password.length) {
-                    ret['message'] = 'Make sure to use digits and lower- '
-                        +'&amp; uppercase characters.';
-                    return ret;
-                }
-                else {
-                    ret['message'] = 'Make sure to use digits and lower- '
-                        + '&amp; uppercase characters. Still need '
-                        + (passlenreq - password.length) + ' chars.';
-                    return ret;
-                }
+            else if (passlenreq <= password.length) {
+                ret['message'] = 'Make sure to use digits and lower- '
+                    +'&amp; uppercase characters.';
+                return ret;
             }
-            else
-                return {
-                    'valid': true,
-                    'points': points,
-                    'message': 'Password is secure.'
-                };
+            else {
+                ret['message'] = 'Make sure to use digits and lower- '
+                    + '&amp; uppercase characters. Still need '
+                    + (passlenreq - password.length) + ' chars.';
+                return ret;
+            }
         }
+        else
+            return {
+                'valid': true,
+                'points': points,
+                'message': 'Password is secure.'
+            };
     }
-
-    function passvigorReport()
-    {
-        var check = passvigor($('#password').val());
-        $('#passvigor-msg').html(check["message"]);
-        $('#passvigor-points').html(check["points"] + '%');
-    }
-
-    $('#password').on('input', passvigorReport);
-});
+}
