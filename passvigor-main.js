85db338471fedd746c9c168eafaf60a7c3eb4fbf
Benjamin Althues <benjamin@babab.nl>
2013-12-08 22:51:42 +0100

    Refactor relscore(); rm obsolete else statement

--- passvigor-main.js
+++ passvigor-main.js
@@ -26,70 +26,61 @@ function passvigor(password)
 
     function relscore(password)
     {
-        var lowercase = /[a-z]/
-        var uppercase = /[A-Z]/
-        var digits = /[0-9]/
-        var special = /[!@#$%^&*()_\-<>?/\\\[\]]/
+        var validsum = 26 + 26 + 10;
         var factor = 0;
 
-        if (lowercase.test(password))
-            factor += 26 / (26 + 26 + 10);
-        if (uppercase.test(password))
-            factor += 26 / (26 + 26 + 10);
-        if (digits.test(password))
-            factor += 10 / (26 + 26 + 10);
-        if (special.test(password))
-            factor += 9 / (26 + 26 + 10);
+        if (/[a-z]/.test(password)) factor += 26 / validsum;
+        if (/[A-Z]/.test(password)) factor += 26 / validsum;
+        if (/[0-9]/.test(password)) factor += 10 / validsum;
+        if (/[!@#$%^&*()_]/.test(password)) factor +=  9 / validsum;
         return factor;
-
     }
 
     if ($.inArray(password, passvigor_wordlist) != -1) {
         ret['message'] = 'Unsecure password: very commonly used.';
         return ret;
     }
-    else {
-        if (password.length == 0) {
-            ret['message'] = 'Enter a password.';
+
+    if (password.length == 0) {
+        ret['message'] = 'Enter a password.';
+        return ret;
+    }
+
+    var relp = relscore(password);
+    var currp = Math.round((password.length / passlenreq) * 100);
+    var points = 0;
+    if (relp < 1 && currp > (relp * 100))
+        points = Math.round(relp * 100);
+    else
+        points = Math.ceil(currp * relp);
+
+    if (points >= (maxstrength + 1))
+        points = maxstrength;
+
+    ret['points'] = points;
+
+    if (points < 100) {
+        if (relp >= 1) {
+            ret['message'] = 'Still need '
+                    + (passlenreq - password.length) + ' chars.';
             return ret;
         }
-
-        var relp = relscore(password);
-        var currp = Math.round((password.length / passlenreq) * 100);
-        var points = 0;
-        if (relp < 1 && currp > (relp * 100))
-            points = Math.round(relp * 100);
-        else
-            points = Math.ceil(currp * relp);
-
-        if (points >= (maxstrength + 1))
-            points = maxstrength;
-
-        ret['points'] = points;
-
-        if (points < 100) {
-            if (relp >= 1) {
-                ret['message'] = 'Still need '
-                        + (passlenreq - password.length) + ' chars.';
-                return ret;
-            }
-            else if (passlenreq <= password.length) {
-                ret['message'] = 'Make sure to use digits and lower- '
-                    +'&amp; uppercase characters.';
-                return ret;
-            }
-            else {
-                ret['message'] = 'Make sure to use digits and lower- '
-                    + '&amp; uppercase characters. Still need '
-                    + (passlenreq - password.length) + ' chars.';
-                return ret;
-            }
+        else if (passlenreq <= password.length) {
+            ret['message'] = 'Make sure to use digits and lower- '
+                +'&amp; uppercase characters.';
+            return ret;
         }
-        else
-            return {
-                'valid': true,
-                'points': points,
-                'message': 'Password is secure.'
-            };
+        else {
+            ret['message'] = 'Make sure to use digits and lower- '
+                + '&amp; uppercase characters. Still need '
+                + (passlenreq - password.length) + ' chars.';
+            return ret;
+        }
     }
+    else
+        return {
+            'valid': true,
+            'points': points,
+            'message': 'Password is secure.'
+        };
 }
